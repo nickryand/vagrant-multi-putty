f64a8f5ef5707361d34064dd6805bd9fdaa5d81e
Nick Downs <nickryand@gmail.com>
2013-04-23 01:25:05 -0700

    Updated modal mode to support multi-putty windows to be spun up prior to blocking call.

--- lib/vagrant-multi-putty/command.rb
+++ lib/vagrant-multi-putty/command.rb
@@ -13,8 +13,8 @@ module VagrantMultiPutty
           options[:plain_auth] = p
         end
 
-        opts.on("-m", "--modal", "Block on the putty child process (multi-vm processes will open one after another)") do
-          options[:modal] = true
+        opts.on("-m", "--modal", "Block until all spawned putty processes have exited") do |m|
+          options[:modal] = m
         end
 
         opts.separator ""
@@ -33,16 +33,24 @@ module VagrantMultiPutty
       # (if any) will be the VM names to log into.
       argv = argv - putty_args
 
+      pids = []
       # Since putty is a program with a GUI window, we can perform a spawn and
       # detach the process from vagrant.
       with_target_vms(argv) do |vm|
         @logger.info("Launching putty session to: #{vm.name}")
-        putty_connect(vm, putty_args, plain_auth=options[:plain_auth])
+        child_pid = putty_connect(vm, putty_args, options)
+        pids << child_pid unless child_pid.nil?
       end
+
+      # Modal like behavior
+      unless pids.empty?
+        pids.each { |child| Process.waitpid(child) }
+      end
+
       return 0
     end
 
-    def putty_connect(vm, args, plain_auth=False)
+    def putty_connect(vm, args, options={})
       # This isn't called by vagrant automatically.
       vm.config.putty.finalize!
 
@@ -56,26 +64,27 @@ module VagrantMultiPutty
       @logger.debug("Putty Private Key: #{pk_path}")
 
       # Load options from machine ssh_info.
-      options = [ssh_info[:host]]
+      ssh_options = [ssh_info[:host]]
       # config.putty.username overrides the machines ssh_info username.
-      options += ["-l", vm.config.putty.username || ssh_info[:username]]
-      options += ["-P", ssh_info[:port].to_s]
-      options += ["-i", pk_path] if !plain_auth
-      options += ["-X"] if ssh_info[:forward_x11]
-      options += ["-A"] if ssh_info[:forward_agent]
+      ssh_options += ["-l", vm.config.putty.username || ssh_info[:username]]
+      ssh_options += ["-P", ssh_info[:port].to_s]
+      ssh_options += ["-i", pk_path] unless options[:plain_auth]
+      ssh_options += ["-X"] if ssh_info[:forward_x11]
+      ssh_options += ["-A"] if ssh_info[:forward_agent]
 
       # Add in additional args from the command line.
-      options.concat(args) if !args.nil?
+      ssh_options.concat(args) if !args.nil?
 
       # Spawn putty and detach it so we can move on.
       @logger.debug("Putty cmd line options: #{options.to_s}")
-      pid = spawn("putty", *options)
+      pid = spawn("putty", *ssh_options)
       @logger.debug("Putty Child Pid: #{pid}")
-      if vm.config.modal
-        Process.waitpid(pid)
+      if options[:modal]
+        return pid
       else
         Process.detach(pid)
       end
+      nil
     end
   end
 end
